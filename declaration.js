5332dba4b9b2e488381875bc6c12ced45027b261
zy2412505039 <2497334128@qq.com>
2026-03-06 15:13:38 +0800

    first commit

---
+++ declaration.js
@@ -0,0 +1,29 @@
+let year;
+year = 2026;
+let month = 3;
+const day = 20;
+var hour = 12;
+console.log(year, '++++++++++++++');
+console.log(month, '----------------');
+console.log(day, '//////------');
+const int = 10;
+const float = 10.1;
+console.log(int, '////////////');
+console.log(float, '/////////////'); 
+const str1 = 'syxy';
+const str2 = 'blockchain';
+const str3 = str1 + str2;
+console.log(str3, '++++++++++-*-*-*-');
+console.log(typeof str3, 'str3的类型');
+const bool1 = true;
+const bool2 = false;
+console.log(bool1, '12121');
+console.log(bool2, '323232');
+const arr1 = [1, 2, 3, 4, 5];
+console.log(arr1, '数组arr1');
+console.log(typeof arr1, '**************');
+const obj1 = {name:'syxy', age:18};
+console.log(obj1, '对象obj1');
+console.log(typeof obj1, '**************');
+
+
